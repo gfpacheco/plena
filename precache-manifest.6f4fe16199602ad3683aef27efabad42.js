e250526d10be2265b3cbb7880b68cd32ebdcb954
Guilherme Pacheco <guilherme.f.pacheco@gmail.com>
2019-10-12 00:18:09 -0400

    Updates

---
+++ precache-manifest.6f4fe16199602ad3683aef27efabad42.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/plena/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "a1f231770140e47fc3e0",
+    "url": "/plena/static/js/app.5f92ffe6.chunk.js"
+  },
+  {
+    "revision": "f906b7697d553a660742d65fd6482ed0",
+    "url": "/plena/static/js/runtime~app.7a08e3c1.js.gz"
+  },
+  {
+    "revision": "38b4e676388d034cb6c263c012d48757",
+    "url": "/plena/static/js/app.5f92ffe6.chunk.js.gz"
+  },
+  {
+    "revision": "ca5b9d55e9e6fe13772e",
+    "url": "/plena/static/js/runtime~app.7a08e3c1.js"
+  },
+  {
+    "revision": "020fd499a2d2bc45c4c3b6021e472b43",
+    "url": "/plena/register-service-worker.js"
+  },
+  {
+    "revision": "e15324dcd02fc3b037b2768a36b9aa26",
+    "url": "/plena/static/js/2.a7d6f454.chunk.js.gz"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/plena/serve.json"
+  },
+  {
+    "revision": "c6aef942e3668158ec29d4adcb2e768f",
+    "url": "/plena/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/plena/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/plena/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "5a293a273bee8d740a045d9922b9a9ae",
+    "url": "/plena/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "bd56db0844d50841ee6e0b69eb5e794d",
+    "url": "/plena/expo-service-worker.js.gz"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/plena/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "97398a60cc0f97736734",
+    "url": "/plena/static/js/2.a7d6f454.chunk.js"
+  },
+  {
+    "revision": "872545dde71de3842234bf6afe80c4cb",
+    "url": "/plena/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "7a7bc7ead25db795e58b336f04d2624c",
+    "url": "/plena/favicon.ico"
+  },
+  {
+    "revision": "5e695e96a003a79f7f97060bf49409a9",
+    "url": "/plena/expo-service-worker.js"
+  },
+  {
+    "revision": "02d2a3d936bec851930bcc66865742fe",
+    "url": "/plena/manifest.json"
+  },
+  {
+    "revision": "17b6a36b3f5302a232b62272a8b20f87",
+    "url": "/plena/index.html"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/plena/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "6beba7e6834963f7f171d3bdd075c915",
+    "url": "/plena/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/plena/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/plena/./fonts/AntDesign.ttf"
+  }
+];
