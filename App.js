81172aaf17cc68c4849a0cb7b63e8ab6a01c7b36
Guilherme Pacheco <guilherme.f.pacheco@gmail.com>
2019-10-12 00:42:09 -0400

    Fix body height on the web

--- App.js
+++ App.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import { Platform } from 'react-native';
 import Chat from './src/components/Chat';
+
+if (Platform.OS === 'web') {
+  document.body.style.height = '100%';
+}
 
 export default function App() {
   return <Chat />;
